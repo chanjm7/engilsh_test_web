5b74f41482c70d3e2c0a514ab180038bd10c5164
chanjm7 <dlcksals77@gmail.com>
2021-11-12 13:16:14 +0900

    add engilsh test web project setting

---
+++ src/main/java/hello/engilshtest/EnglishTestWebApplication.java
@@ -0,0 +1,13 @@
+package hello.engilshtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishTestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishTestWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/engilshtest/EngilshTestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.engilshtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngilshTestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
